a600f540060c03b0ae376cedf22615006dc76ce4
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2022-12-25 03:42:06 -0300

    refresh

--- aula002/ClassUsuario.php
+++ aula002/ClassUsuario.php
@@ -1,11 +1,11 @@
 <?php
     class Usuario{
-    public $Nombre;
-    public $Email;
-    public $Tipo;
+    private $Nombre; //Neste caso quando e private devemos trabalhar com get e set
+    private $Email;
+    private $Tipo;
     private $Clave;
     protected $FechaRegistro;
-    static $Estado = "Activo";
+    static $Estado = "Activado";
 
     function __construct(string $nombre, string $email, string $tipo)
     {
@@ -13,6 +13,24 @@
         $this->Email = $email;
         $this->Tipo = $tipo;
         $this->Clave = rand();
+        $this->FechaRegistro = date('d/m/y H:m:s');
+        // $this->Estado = $estado;
     }
+
+    public function getperfil(){
+        echo "<br><br><hr>O seu nombre é: ".$this->Nombre;
+        echo "<br><br><hr>O seu email é: ".$this->Email;
+        echo "<br><br><hr>A sua função é: ".$this->Tipo;
+        echo "<br><br><hr>A sua chave é: ".$this->Clave;
+        echo "<br><br><hr>O seu estado: ".self::$Estado;
+        echo "<br><br><hr>A sua da de inscrito: " . $this->FechaRegistro."<br><br>";
     }
+
+    public function seteditarDatos(string $pass){
+        $this->Clave = $pass;
+    }
+
+
+    }//Fim da classUsuario
+ 
 ?>
--- aula002/usuario.php
+++ aula002/usuario.php
@@ -1,5 +1,13 @@
 <?php
-require_once('ClassUsuario.php');
-    echo 'konye02n2';
+    require_once('ClassUsuario.php');
+    $objUsuarioUno = new Usuario("Wilhem Wundt Maxime", "wilhemmaxime90@gmail.com", "Administrador");
 
+    $objUsuarioDos = new Usuario("Adventia hyppolite", "adventiahyppolite94@gmail.com", "Vendedora");
+
+    
+    echo $objUsuarioUno -> getperfil();
+    echo $objUsuarioDos -> getperfil();
+    $objUsuarioDos-> seteditarDatos("1234"); //Realizamos a troca da clave por aqui
+    print "<br><br><br>";
+    echo $objUsuarioDos -> getperfil();
 ?>
